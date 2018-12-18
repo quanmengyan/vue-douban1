4a06ab4fd33f92f416733a77555a8a99a2f588e8
quanmengyan <1206307482@qq.com>
2018-12-18 19:21:46 +0800

    dml

---
+++ precache-manifest.ce237b18487cb166b01122acb014e945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "2894918c2d3fad3f8337",
+    "url": "/js/chunk-vendors.8e475858.js"
+  },
+  {
+    "revision": "55597407984ff8c0df30",
+    "url": "/js/chunk-d7efa8de.71896083.js"
+  },
+  {
+    "revision": "28fd569f2019beb8c258",
+    "url": "/js/app.3615dfac.js"
+  },
+  {
+    "revision": "8933cd2d023102e7caaac5d2ca60d03d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55597407984ff8c0df30",
+    "url": "/css/chunk-d7efa8de.28c21025.css"
+  },
+  {
+    "revision": "28fd569f2019beb8c258",
+    "url": "/css/app.a81ce4f6.css"
+  }
+];
